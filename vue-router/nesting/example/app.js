fa107dae42e5dc26ac1af7f775bd95952b9dda65
dhershman1 <dustinh17@gmail.com>
2018-08-21 16:51:39 -0400

    Built nesting example code

---
+++ vue-router/nesting/example/app.js
@@ -0,0 +1,38 @@
+const Blog = {
+  template: `
+    <div class="user">
+      <h2>Blog {{ $route.params.id }}</h2>
+      <router-view></router-view>
+    </div>
+  `
+}
+
+const BlogDash = { template: '<div>Dash</div>' }
+const BlogFollowers = { template: '<div>Followers</div>' }
+const BlogPosts = { template: '<div>Posts</div>' }
+
+const router = new VueRouter({
+  routed: [
+    {
+      path: '/blog/:id',
+      component: Blog,
+      children: [
+        {
+          path: '',
+          component: BlogDash
+        },
+        {
+          path: 'followers',
+          component: BlogFollowers
+        },
+        {
+          path: 'posts',
+          component: BlogPosts
+        }
+      ]
+    }
+  ]
+})
+
+
+const app = new Vue({ router }).$mount('#app')
